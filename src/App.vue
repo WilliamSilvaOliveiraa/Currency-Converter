b523769d6d02f37976de2a05b70dc9bd705bc36f
WilliamSilva <williamsilva20062005@gmail.com>
2023-12-17 17:29:56 -0300

    Ultimas alterações de lógica e correções de CSS

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,46 @@
 
 <template>
     <div id="app">
-        <Conversor moeda-a="USD" moeda-b="BRL"></Conversor>
+        <h1 class="text-center">Conversor de Moedas</h1>
+        <div class="linha">
+            <Conversor
+                :moedaA="moedaA"
+                :moedaB="moedaB"
+                :cotacao="cotacaoReaisToDolar"
+            ></Conversor>
+
+            <Conversor
+                :moedaA="moedaB"
+                :moedaB="moedaA"
+                :cotacao="cotacaoDolarToReais"
+            ></Conversor>
+        </div>
+        <div class="linha">
+            <Conversor
+                :moedaA="moedaA"
+                :moedaB="moedaC"
+                :cotacao="cotacaoReaisToDolarCad"
+            ></Conversor>
+
+            <Conversor
+                :moedaA="moedaC"
+                :moedaB="moedaA"
+                :cotacao="cotacaoDolarCadToReais"
+            ></Conversor>
+        </div>
+        <div class="linha">
+            <Conversor
+                :moedaA="moedaA"
+                :moedaB="moedaD"
+                :cotacao="cotacaoReaisToEuro"
+            ></Conversor>
+
+            <Conversor
+                :moedaA="moedaD"
+                :moedaB="moedaA"
+                :cotacao="cotacaoEuroToReais"
+            ></Conversor>
+        </div>
     </div>
 </template>
 
@@ -16,8 +55,17 @@ export default {
     },
     data() {
         return {
-            moedaA: 'USD', // Substitua pelos valores desejados
-            moedaB: 'EUR', // Substitua pelos valores desejados
+            moedaA: 'BRL', // Substitua pelos valores desejados
+            moedaB: 'USD', // Substitua pelos valores desejados
+            moedaC: 'CAD', // Substitua pelos valores desejados
+            moedaD: 'EUR', // Substitua pelos valores desejados
+
+            cotacaoDolarToReais: 4.94,
+            cotacaoReaisToDolar: 0.2, // Defina outro valor de cotação
+            cotacaoDolarCadToReais: 3.7, // Defina outro valor de cotação
+            cotacaoReaisToDolarCad: 0.27, // Defina outro valor de cotação
+            cotacaoReaisToEuro: 0.19, // Defina outro valor de cotação
+            cotacaoEuroToReais: 5.39, // Defina outro valor de cotação
         };
     },
 };
@@ -25,11 +73,21 @@ export default {
 
 <style>
 #app {
+    margin: auto;
+    max-width: 900px;
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+    margin-bottom: 100px;
     color: #2c3e50;
     margin-top: 60px;
+}
+.linha {
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-around;
+}
+.text-center {
+    text-align: center;
 }
 </style>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="conversor">
         <h2>{{ moedaA }} Para {{ moedaB }}</h2>
-        <input type="text" />
-        <input type="button" value="Converter" />
+        <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA" />
+        <input type="button" value="Converter" v-on:click="converter" />
+        <h2>{{ moedaB_value }}</h2>
     </div>
 </template>
 
 <script>
 export default {
     name: 'ConversorComponent',
-    props: ['moedaA', 'moedaB'],
+    props: ['moedaA', 'moedaB', 'cotacao'],
+    data() {
+        return {
+            moedaA_value: '',
+            moedaB_value: 0,
+        };
+    },
+    methods: {
+        converter() {
+            const valorMoedaA = parseFloat(this.moedaA_value);
+            if (isNaN(valorMoedaA)) {
+                return;
+            }
+
+            // Agora, a variável 'cotacao' é uma propriedade configurável pelo componente pai
+            this.moedaB_value = (valorMoedaA * this.cotacao).toFixed(2);
+        },
+    },
 };
 </script>
 
 <style scoped>
-/* prettier-ignore */
+.conversor {
+    padding: 20px;
+    max-width: 400px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    border: 1px solid #c2c2c2;
+}
 </style>
